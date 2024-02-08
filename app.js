f7871c57de71bd9daf9b48e2d166ca97e97c8942
João Vieira <joaovieirapt17@gmail.com>
2024-02-08 20:52:09 +0000

    feat(auth): add 'currentUser' to track if there is a user loggedIn

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // FLASH MIDDLEWARE
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user; // track 'current user'
   res.locals.success = req.flash("success"); // local 'success' variable
   res.locals.error = req.flash("error"); // local 'error' variable
   next();
