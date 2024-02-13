174baf8981974d3b369dd6ea23df231a4766ce19
João Vieira <joaovieirapt17@gmail.com>
2024-02-13 23:54:44 +0000

    style: add some styles into the pages

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   style: "mapbox://styles/mapbox/dark-v11",
   center: [-103.5917, 40.6699],
   zoom: 3,
